d44d91c85e52583307033a3feb231b17070e3c04
Fojiuma <fojiumajames@gmail.com>
2024-09-22 17:55:19 +0100

    init Calculator

---
+++ test.js
@@ -0,0 +1,22 @@
+// let a = 3;
+// let b = 3;
+// a == b ? console.log("Match") : console.log("No match");
+// var pens;
+pens = ["red", "blue", "green", "orange"];
+pens = new Array("red", "blue", false, 343); 
+
+
+// pens[3] = "purple";
+
+// console.log(pens);
+
+// var fourthpen = pens[3];
+
+// console.log(fourthpen);
+
+// pens.reverse();
+
+//  pens.splice(3, 1);
+
+// console.log("array length: ", pens.length)
+console.log(pens);
