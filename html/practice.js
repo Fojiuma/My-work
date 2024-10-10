6317e6a6b8a14b71137acaf16a407ad0dc56bf72
Fojiuma <fojiumajames@gmail.com>
2024-10-10 11:35:14 +0100

    10-10-24

--- html/practice.js
+++ html/practice.js
@@ -1,3 +1,100 @@
+// let firstcard = 6;
+// let secondcard = 9;
+// let cards = [firstcard, secondcard];
+// let hasBlackJack = false;
+// let isAlive = true;
+// let sum = firstcard + secondcard;
+// let message = ""
+
+// let CardsEl = document.getElementById("cards-el")
+
+
+// // let sumEl = documnet.getElementById("sum-el")
+// let sumEl = document.querySelector("#sum-el")
+// let messageEl = document.getElementById("message-el")
+
+// function startGame() {
+//     renderGame()
+// }
+// function renderGame() {
+//     CardsEl.textContent = "Cards: " + card[0] + " " + card[1]
+//     sumEl.textContent = "Sum: " + sum
+//     if(sum < 21){
+//         message = "Do you want to draw a new card?"
+//     }
+//     else if(sum === 21){
+//         message = "You've got black jack"
+//     hasBlackJack = true;
+//     }
+//     else if(sum > 21){
+//         message = "YOU ARE OUT OF THE GAME!"
+//         isAlive = false 
+//     }
+//     messageEl.textContent = message
+//    }
+
+//   function newCard(){
+//     let card = 6;
+//     sum = sum + card;
+//     cards.push(card);
+//     renderGame();
+//   }
+  
+for (let count = 10; count < 21; count += 1){
+    console.log(count)
+}
+
+for (let i = 10; i < 101;i += 10){
+console.log (i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // let firstname = "FOJIUMA"
 // let lastname = " LANGAYA"
 // let fullName = lastname + firstname;
@@ -26,38 +123,33 @@
 // function error() {
 //      errorEl.textContent = "Something went wrong, please try again"
 // }
+// let num1 = 8;
+// let num2 = 2;
+// document.getElementById("num1-el").textContent = num1;
+// document.getElementById("num2-el").textContent = num2;
 
 
+// let sumEl = document.getElementById("sum-el");
 
+//  function add() {
+//      let result = num1 +num2;
+//      sumEl.textContent = "Sum: " + result;
+//  }
 
+//  function subtract() {
+//     let result = num1 - num2;
+//     sumEl.textContent = "subtraction: " + result
+//  }
 
+//  function divide() {
+//     let result = num1 / num2;
+//     sumEl.textContent = "division: " + result;
+//  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//  function multiply() {
+//     let result = num1 * num2;
+//     sumEl.textContent = "Multiplication: " + result;
+//  }
 
 
 // let countEl = document.getElementById("count-el")
