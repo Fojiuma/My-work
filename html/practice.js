f94837e3450edc01b988ff445b5b0a2c70b749fe
Fojiuma <fojiumajames@gmail.com>
2024-10-14 11:58:15 +0100

    commit

--- html/practice.js
+++ html/practice.js
@@ -1,52 +1,122 @@
-// let firstcard = 6;
-// let secondcard = 9;
-// let cards = [firstcard, secondcard];
-// let hasBlackJack = false;
-// let isAlive = true;
-// let sum = firstcard + secondcard;
-// let message = ""
+let firstcard = getRandomCard();
+let secondcard = getRandomCard();
+let cards = [firstcard, secondcard];
+let hasBlackJack = false;
+let isAlive = true;
+let sum = firstcard + secondcard;
+let message = ""
+let CardsEl = document.getElementById("cards-el")
 
-// let CardsEl = document.getElementById("cards-el")
+
+// console.log(getRandomCard)
+
+// let sumEl = documnet.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+let messageEl = document.getElementById("message-el")
+
+function getRandomCard() {
+    let randomNmunber =  Math.floor(Math.random() * 13 ) + 1
+    if (randomNmunber > 10) {
+        return 10
+    }else if(randomNmunber === 1) {
+        return 11
+    }else {
+        return randomNmunber
+    }
+    }
+
+function startGame() {
+    renderGame()
+}
+function renderGame() {
+    CardsEl.textContent = "Cards: " 
+    
+for(i = 0; i < cards.length; i++ ){
+    CardsEl.textContent += cards[i] + " "
+}
 
 
-// // let sumEl = documnet.getElementById("sum-el")
-// let sumEl = document.querySelector("#sum-el")
-// let messageEl = document.getElementById("message-el")
+    sumEl.textContent = "Sum: " + sum
+    if(sum < 21){
+        message = "Do you want to draw a new card?"
+    }
+    else if(sum === 21){
+        message = "You've got black jack"
+    hasBlackJack = true;
+    }
+    else if(sum > 21){
+        message = "YOU ARE OUT OF THE GAME!"
+        isAlive = false 
+    }
+    messageEl.textContent = message
+   }
 
-// function startGame() {
-//     renderGame()
+  function newCard(){
+    let card = getRandomCard();
+    sum = sum + card;
+    cards.push(card);
+    renderGame();
+  }
+
+let randomnumber = 
+console.log(randomnumber)
+
+
+
+
+// function rollDice() {
+//     let flooredNumber = Math.floor(Math.random() * 7 ) + 1
+//     return flooredNumber
 // }
-// function renderGame() {
-//     CardsEl.textContent = "Cards: " + card[0] + " " + card[1]
-//     sumEl.textContent = "Sum: " + sum
-//     if(sum < 21){
-//         message = "Do you want to draw a new card?"
-//     }
-//     else if(sum === 21){
-//         message = "You've got black jack"
-//     hasBlackJack = true;
-//     }
-//     else if(sum > 21){
-//         message = "YOU ARE OUT OF THE GAME!"
-//         isAlive = false 
-//     }
-//     messageEl.textContent = message
-//    }
 
-//   function newCard(){
-//     let card = 6;
-//     sum = sum + card;
-//     cards.push(card);
-//     renderGame();
-//   }
+// console.log(rollDice())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
-for (let count = 10; count < 21; count += 1){
-    console.log(count)
-}
+// let player1Time = 102
+// let player2Time = 107
 
-for (let i = 10; i < 101;i += 10){
-console.log (i)
-}
+// function getFastestRaceTime() {
+//     if (player1Time < player2Time) {
+//         return player1Time
+//     }else if(player1Time > player2Time){
+//         return  player2Time
+//     }else{
+//         return player1Time
+//     }
+// }
+
+// let fastestRace = getFastestRaceTime()
+
+// console.log(fastestRace)
+
+// function totalRaceTime() {
+//     return player1Time +player2Time
+// }
+
+// let totaltime = totalRaceTime()
+// console.log(totaltime)
 
 
 
@@ -56,6 +126,47 @@ console.log (i)
 
 
 
+
+
+
+
+
+
+
+
+
+
+// for (let count = 10; count < 21; count += 1){
+//     console.log(count)
+// }
+
+// for (let i = 10; i < 101;i += 10){
+// console.log (i)
+// }
+
+//  let messages = [
+//     "hey hows it going",
+//     "im great",
+//     "oya na we go word later",
+
+//  ]
+// //  i < length ensures that everything within the array is logged out to the console including newly added elements
+// for (let i = 0; i < length; i += 1){
+//     // console.log(messages[i])
+
+// }
+// let cards = [7, 3, 9]
+// for (i = 0; i < cards.length; i++){
+//     console.log(cards[i])
+// }
+
+// let sentence = ["hello ", "my ", "name ", "is ", "fojiuma"]
+// let greetingEl = document.getElementById("greeting-el")
+
+
+// for (i = 0; i < sentence.length; i++) {
+//     greetingEl.textContent += sentence[i] + " ";
+// }
 
 
 
