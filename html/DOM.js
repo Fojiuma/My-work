2cb302f12f93049336fcd48d68590baee403d671
Fojiuma <fojiumajames@gmail.com>
2024-11-12 13:40:55 +0100

    tuesday

--- html/DOM.js
+++ html/DOM.js
@@ -4,3 +4,9 @@
 //querySelector()
 //querySelectorAll()
 
+ const firstListItem = document.querySelector('.list-Items');
+
+ console.log(firstListItem.innerText);
+console.log(firstListItem.textContent);
+console.log(firstListItem.innerHTML);
+
