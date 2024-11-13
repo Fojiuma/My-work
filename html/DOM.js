6d3246eed6c05f85968f50283c1c09a9d896bd4f
Fojiuma <fojiumajames@gmail.com>
2024-11-13 09:12:22 +0100

    11-13-24

--- html/DOM.js
+++ html/DOM.js
@@ -4,9 +4,26 @@
 //querySelector()
 //querySelectorAll()
 
- const firstListItem = document.querySelector('.list-Items');
 
- console.log(firstListItem.innerText);
-console.log(firstListItem.textContent);
-console.log(firstListItem.innerHTML);
+//Creating new elements
+//  const ul = document.querySelector('ul')
+//  const li = document.createElement('li')
+
+ //adding element through javascript
+//  ul.append(li)
+
+//modifying the text
+// li.innerText = "X-men"
+
+//Styling the Attributes of the new element
+// li.setAttribute('id', 'mainheading');
+//gives the new attribute the styling of whatever is in the second apostrophe
+// li.removeAttribute('id');
+//remove the attribute 
+
+// li.remove();
+//remove the element from the html elements
+
+
+
 
