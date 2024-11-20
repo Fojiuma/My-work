baba94bcf94350398b529f6ecb87f4d8aff063f6
Fojiuma <fojiumajames@gmail.com>
2024-11-21 00:31:57 +0100

    21-11-24\

--- html/DOM.js
+++ html/DOM.js
@@ -3,12 +3,36 @@ let quote = document.querySelector('.quote');
 let person = document.querySelector('.person')
 
 const quotes = [{
-   
+   quote: `"The best way to find yourself is to lose yourself in the
+   service of others."`,
+   person: `mahatma gandhi`
+},{
+  quote: `"if you want to live a happy life, tie it to a goal, not to the people or things."`,
+  person: `albert einstein`
+}, {
+  quote: `"At his best, man is the noblest of all animals; seperated from law and justice he is the worst."`,
+  person: `Aristotle`
+},{
+  quote:`"Your time is limited,  so don't waste it living somone else's life."`,
+  person: `Steve Jobs`
+},{
+  quote:`"Tell me and I forgot. Teach me and I remember. Involve me and I learn. "`,
+  person: `Benjamin Franklin`
+},{
+  quote:`"If you look at what you have in life, you'll always more.If you look at what you don't have in life, you'll never have enough"`,
+  person:`Oprah Winfrey`
 }]
 
 
 
+btn.addEventListener('click', function(){
 
+  let random = Math.floor(Math.random() * quotes.length);
+
+  quote.innerText = quotes[random].quote;
+  person.innerText = quotes[random].person;
+
+})
 
 
 
