eb7f3a97ae05c35208c99e062934979b2d620f67
Fojiuma <fojiumajames@gmail.com>
2024-11-22 15:17:50 +0100

    11-22-24

--- html/DOM.js
+++ html/DOM.js
@@ -1,5 +1,11 @@
+const accordion = document.getElementsByClassName('content-container');
 
+for (i = 0; i < accordion.length; i++ ){
 
+  accordion[i].addEventListener('click,', function () {
+    this.classList.toogle('active');    
+  })
+}
 
 
 
