4f156c970110bbcba0d57b88ae5879fb1aeec231
Fojiuma <fojiumajames@gmail.com>
2024-12-02 11:24:36 +0100

    5-12-24

--- html/DOM.js
+++ html/DOM.js
@@ -1,7 +1,37 @@
+const addTask = document.getElementById('addd-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('inout-task');
 
 
+addTask.addEventListener('click', function(){
+
+    let task = document.createElement('div')
+    task.classList.add('task');
 
 
+    let li  = document.createElement('li');
+    li.innerText = `${inputTask.value}`;
+    task.appendChild(li)
+
+    let checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButtton);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButtton);
+
+    if(inputTask.value === ""){
+        alert('Please Enter a Task');
+
+    }else {
+        taskContainer.appendChild(task);
+    }
+
+
+});
 
 
 
