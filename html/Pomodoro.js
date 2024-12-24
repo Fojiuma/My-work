9ff74ecc7a25216b73508afd1bb704f13047e23b
Fojiuma <fojiumajames@gmail.com>
2024-12-24 23:15:24 +0100

    christmas eve

--- html/Pomodoro.js
+++ html/Pomodoro.js
@@ -1,14 +1,47 @@
-let [seconds, minutes,hours] = [0,0,0];
-let displayTime = document.getElementById("display-Time");
+// let [seconds, minutes,hours] = [0,0,0];
+// let displayTime = document.getElementById("display-Time");
+// let timer;
 
-function increaseStopWatch(){
+// function increaseStopWatch(){
 
 
-if(seconds === 60, seconds++ ){
-    seconds = 0;
-    minutes++;
-}
-if(minutes === 60){
-    minutes = 0;
-    hours++;
-}}
+// if(seconds === 60, seconds++ ){
+//     seconds = 0;
+//     minutes++;
+// }
+// if(minutes === 60){
+//     minutes = 0;
+//     hours++;
+// }}
+
+// function watch(){
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");//to get elements nested in another element
+
+var altTEXT = THEIMAGE.getAttribute("alt");
+
+var captionElement = document.createElement("figcaption")
+
+captionElement.appendChild("altTEXT")
