8e666771fe3e20e8bbaf67163d37c5531200f228
Fojiuma <fojiumajames@gmail.com>
2024-12-31 12:44:49 +0100

    last day of the year

--- html/script.js
+++ html/script.js
@@ -1,42 +1,26 @@
-const notesContainer = document.querySelector(".notes-container");
-const createBtn = document.querySelector(".btn");
-let notes = document.querySelectorAll(".input-box");
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
 
-function showNotes() {
-  notesContainer.innerHTML = localStorage.getItem("notes");
+var date = new Date();    
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+console.log("Hours: "+ hr + "; " + "Minutes: " + min + ";" + "Seconds: " + sec) 
+
+var hrPosition = hr*360/12 + (min * (360/60 )/12);
+var minPosition = min*360/60 + (sec * (360/60)/60);
+var secPosition = sec*360/60;
+
+function getTheDate(){
+    hrPosition = hrPosition + (3/360);
+    minPosition = minPosition + (6/60);
+    secPosition = secPosition + 6;
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
+    MINUTEHAND.style.transform = "rotate(" + minPosition +"deg)"
+    SECONDHAND.style.transform = "rotate(" + secPosition +"deg)"
 }
-showNotes();
 
-function updateStorage() {
-  localStorage.setItem("notes", notesContainer.innerHTML);
-}
+var interval = setInterval(getTheDate, 1000)
 
-createBtn.addEventListener("click", () => {
-  let inputBox = document.createElement("p");
-  let img = document.createElement("img");
-  inputBox.className = "input-box";
-  inputBox.setAttribute("contenteditable", "true");
-  img.src = "images/delete.png";
-  notesContainer.appendChild(inputBox).appendChild(img);
-});
-
-notesContainer.addEventListener("click", function (e) {
-  if (e.target.tagName === "IMG") {
-    e.target.parentElement.remove();
-    updateStorage();
-  } else if (e.target.tagName === "p") {
-    notes = document.querySelectorAll(".input-box");
-    notes.forEach((nt) => {
-      nt.onkeyup = function () {
-        updateStorage();
-      };
-    });
-  }
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    document.execCommand("insertLinebreak");
-    event.preventDefault();
-  }
-});
