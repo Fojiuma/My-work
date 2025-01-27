b1f3ca3f7b194d87789278b93db3884835b4bba5
Fojiuma <fojiumajames@gmail.com>
2025-01-27 12:26:14 +0100

    ass

--- html/project.js
+++ html/project.js
@@ -2,10 +2,3 @@ const login = document.getElementById("#button");
 const username = document.querySelector("#Username");
 const password = document.querySelector("#password");
 
-
-login.addEventListener("click", function enter(){
-    let success = document.createElement('p')
-    if(password && username > 6) {
-        login.appendChild(success);
-    }
-})
