da78039f2e94fffeaeb703610b81043bb6073879
Fojiuma <fojiumajames@gmail.com>
2025-01-27 15:43:38 +0100

    workday

--- html/project.js
+++ html/project.js
@@ -1,4 +1,14 @@
-const login = document.getElementById("#button");
-const username = document.querySelector("#Username");
-const password = document.querySelector("#password");
+const login = document.getElementById("button");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const mainDiv = document.getElementById("main-div");
 
+login.addEventListener('click', () =>{
+    if(username.value.length < 6){
+        console.log("Your username should be greater than 6 chars");
+    }   else if (username.value == "Fojiuma"|| password.value == "fojiuma04"){
+    console.log("Successful Login")
+   }
+})
+
+mainDiv.style = "red"
