e19baa92818f059421bebb0c9eda0611e67507e6
Fojiuma <fojiumajames@gmail.com>
2025-01-29 03:42:16 +0100

    wednesday

--- html/project.js
+++ html/project.js
@@ -4,13 +4,15 @@ const password = document.getElementById("password");
 const mainDiv = document.getElementById("main-div");
 const successMessage = document.getElementById("success-message");
 
-login.addEventListener('click', () =>{
-    if(username.value.length < 6){
-        console.log("Your username should be greater than 6 chars");
-    }   else if (username.value == "Fojiuma"|| password.value == "fojiuma04"){
-    console.log("Successful Login")
-   }
-   successMessage.textContent = "Login Successful"
-   successMessage.style.color = "green"
-})
-
+login.addEventListener("click", () => {
+  if (username.value.length && password.value.length < 6 && username.value == "" || password.value == "") {
+    successMessage.textContent = "your blah blah blah";
+    successMessage.style.color = "red";
+    successMessage.style.fontSize = "1.5rem";   
+  } else {
+    successMessage.textContent = "successful";
+    successMessage.style.color = "green";
+    successMessage.style.fontSize = "1.2rem";
+  }
+  
+});
