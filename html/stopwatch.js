5dd3eba2b651485eb759001885e9feebb941d1ad
Fojiuma <fojiumajames@gmail.com>
2025-04-10 06:16:02 +0100

    11-04-25

--- html/stopwatch.js
+++ html/stopwatch.js
@@ -1,1 +1,23 @@
+let [seconds, minutes, hours] = [0, 0, 0];
+let displayTime = document.getElementById("displaytime");
+let timer = null;
 
+function stopWatch() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+  displayTime.innerHTML = hours + ": " + minutes + ": " + seconds;
+}
+function watchStart() {
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  setInterval(stopWatch, 1000);
+}
+ 
