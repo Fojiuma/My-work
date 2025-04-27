3d55d050e9de29ae2f4354b5795255496c9e4229
Fojiuma <fojiumajames@gmail.com>
2025-04-27 10:50:36 +0100

    2-05-25

--- html/ToDo.js
+++ html/ToDo.js
@@ -1,5 +1,5 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const listContainer = document.querySelector("list-container");
 const Info = document.getElementById("alert-info");
 function addTask(){
     if (inputBox.value === ""){
@@ -8,5 +8,28 @@ function addTask(){
        let li = document.createElement("li");
        li.innerHTML = inputBox.value;
        listContainer.appendChild("li");
+       let span = document.createElement("span");
+       span.innerHTML = "\u00d7";
+       li.append(span);
     }
+    inputBox.value = "";
+    saveData();
 }
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classlist.toogle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
