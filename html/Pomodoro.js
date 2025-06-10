06dad2f65a6bcf226f1549d8637fcb4a119759d8
Fojiuma <fojiumajames@gmail.com>
2025-06-10 20:10:34 +0100

    initial commit

--- html/Pomodoro.js
+++ html/Pomodoro.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const longestBreak = document.querySelector(".longest-break");
+const shortBreak = document.querySelector(".short-timer");
+const longBreak = document.querySelector(".long-break");
+const startNumber = document.getElementById("start-number");
+shortBreak.addEventListener("click", function () {
+  startNumber.textContent = `5:00`;
+  document.querySelector("body").style.backgroundColor = "rgb(57, 112, 151);";
+});
