e41c420ec86b272bc634b32884fda3077723e57d
Fojiuma <fojiumajames@gmail.com>
2025-06-13 22:37:31 +0100

    initial commit

--- html/practice.js
+++ html/practice.js
@@ -1,12 +1,32 @@
 // "use strict";
 
-// const restaurant = {
-//   name: "classic Italiano",
-//   location: "Via Angelo Tavanti 23, Firenze, Italy",
-//   categories: ["Italian", "Pizzeria", "Vegeterian", "Organic"],
-//   starterMenu: ["Focaccia", "Bruschetta", "Garlic bread", "Caprese Salad"],
-//   mainMenu: ["Pizza", "Pasta", "Risotto"],
-// };
+const restaurant = {
+  name: "classic Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegeterian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
 
 // const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
