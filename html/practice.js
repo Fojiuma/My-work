bf6c9fdbad007d2940a6952ee5181cd89820af3c
Fojiuma <fojiumajames@gmail.com>
2025-06-14 23:24:09 +0100

    initial commit

--- html/practice.js
+++ html/practice.js
@@ -30,6 +30,22 @@ console.log(name, openingHours, categories);
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 console.log("menu");
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
 // const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 // console.log([i,j]);
