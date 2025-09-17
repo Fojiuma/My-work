c4144ef47b350bde2a96c5fe9ae9604b624d034a
Fojiuma <fojiumajames@gmail.com>
2025-09-17 13:00:20 +0100

    initial commit

---
+++ Password work/password.js
@@ -0,0 +1,33 @@
+const displayBox = document.getElementById("password-display");
+
+const length = 12;
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = `!{},./;[]=-)(*&^%$#@#)`;
+
+const allChars = lowerCase + upperCase + numbers + symbols;
+
+const generateButton = document
+  .getElementById("generate")
+  .addEventListener("click", function () {
+    let password = "";
+    password +=
+      lowerCase[Math.floor(Math.random(lowerCase) * lowerCase.length)];
+    password +=
+      upperCase[Math.floor(Math.random(upperCase) * upperCase.length)];
+    password += numbers[Math.floor(Math.random(numbers) * numbers.length)];
+    password += symbols[Math.floor(Math.random(symbols) * symbols.length)];
+
+    while (length > password.length) {
+      password += allChars[Math.floor(Math.random(allChars) * allChars.length)];
+    }
+    displayBox.value = password;
+  });
+
+const copyImg = document
+  .getElementById("copy-img")
+  .addEventListener("click", function () {
+    displayBox.select();
+    document.execCommand("copy");
+  });
