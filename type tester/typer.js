2d3536a7da47da56fc54764d7be51d09198d2a53
Fojiuma <fojiumajames@gmail.com>
2025-09-19 16:52:15 +0100

    final commit

---
+++ type tester/typer.js
@@ -0,0 +1,30 @@
+const displayArea = document.getElementById("typeArea");
+const startOver = document.getElementById("start");
+const starter = document.getElementById("starter");
+
+var timer = [0, 0, 0, 0];
+
+function runTimer() {
+  let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+  starter.innerHTML = currentTime;
+  timer[3]++;
+
+  timer[0] = Math.floor(timer[3] / 100 / 60);
+  timer[1] = Math.floor(timer[3] / 100) - timer[0] * 60;
+  timer[2] = Math.floor(timer[3] - timer[1] * 100) - timer[0] * 6000;
+}
+function start() {
+  let textEntered = displayArea.value.length;
+  if (textEntered === 0) {
+    setInterval(runTimer, 10);
+  }
+}
+function SpellCheck() {
+  let textEnterd = displayArea.value;
+}
+function reset() {
+  console.log("key pressed");
+}
+displayArea.addEventListener("keypress", start, false);
+displayArea.addEventListener("keyup", spellCheck, false);
+startOver.addEventListener(reset, false);
