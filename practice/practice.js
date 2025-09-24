a0290b87fc0af7925799d9fee5f7429492a46357
Fojiuma <fojiumajames@gmail.com>
2025-09-24 23:22:53 +0100

    final commit

--- practice/practice.js
+++ practice/practice.js
@@ -1,8 +1,6 @@
 // "use strict"; enter = \n;
 
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
-console.log(flights.split("+"));
+const flight = "LH234";
 
 // 🔴 Delayed Departure from FAO to TXL (11h25)
 //            Arrival from BRU to FAO (11h45)
